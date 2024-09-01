b497f71df3d4d94f6a84d04fdb299001969fefbb
Samiullah-Shaheen <123831025+Samiullah-Shaheen@users.noreply.github.com>
2024-09-01 10:58:43 +0430

    Add files via upload
    my portofolio

---
+++ assets/js/main.js
@@ -0,0 +1,67 @@
+"use strict";
+
+/**
+ * header sticky & go to top
+ */
+
+const header = document.querySelector("[data-header]");
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 10) {
+    header.classList.add("active");
+    goTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    goTopBtn.classList.remove("active");
+  }
+});
+
+/**
+ * navbar toggle
+ */
+
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+const navbar = document.querySelector("[data-navbar]");
+
+navToggleBtn.addEventListener("click", function () {
+  elemToggleFunc(navToggleBtn);
+  elemToggleFunc(navbar);
+  elemToggleFunc(document.body);
+});
+
+// home text genrator====================
+document.addEventListener("DOMContentLoaded", function () {
+  var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Youtuber", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000, // Correcting 'backDeplay' to 'backDelay'
+    loop: true,
+  });
+});
+
+/**
+ * skills toggle
+ */
+/**
+ * element toggle function
+ */
+
+const elemToggleFunc = function (elem) {
+  elem.classList.toggle("active");
+};
+
+const toggleBtnBox = document.querySelector("[data-toggle-box]");
+const toggleBtns = document.querySelectorAll("[data-toggle-btn]");
+const skillsBox = document.querySelector("[data-skills-box]");
+
+for (let i = 0; i < toggleBtns.length; i++) {
+  toggleBtns[i].addEventListener("click", function () {
+    elemToggleFunc(toggleBtnBox);
+    for (let i = 0; i < toggleBtns.length; i++) {
+      elemToggleFunc(toggleBtns[i]);
+    }
+    elemToggleFunc(skillsBox);
+  });
+}
